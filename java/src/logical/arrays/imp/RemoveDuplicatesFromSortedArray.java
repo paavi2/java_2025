f5741d49a6b91c3d95214eb433a63fdf9cd8b67d
pavithra-prakasam <pavithra.prakasam@games24x7.com>
2025-09-09 07:33:03 +0530

    remove duplicates

---
+++ java/src/logical/arrays/imp/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,27 @@
+package logical.arrays.imp;
+
+import java.util.Arrays;
+
+//Two pointers
+public class RemoveDuplicatesFromSortedArray {
+
+    public static int[] removeDuplicates(int []ar){
+        System.out.println("Input => "+Arrays.toString(ar));
+        int left = 0;
+        int right = 1;
+        while(right < ar.length){
+            if(ar[left] != ar[right]){
+                left = left+1;
+                ar[left] = ar[right];
+            }
+            right++;
+        }
+        int out[] = Arrays.copyOf(ar,left);
+        System.out.println("Output => "+ Arrays.toString(out));
+        return ar;
+    }
+
+    public static void main(String[] args) {
+        removeDuplicates(new int[]{0,1,1,1,2,2,3,5});
+    }
+}
