b3915fbafd15e746726270ebcd96b165ab86f34d
pavithra-prakasam <pavithra.prakasam@games24x7.com>
2025-09-09 09:36:58 +0530

    added palindrome number

--- java/src/logical/arrays/imp/Palindrome_Number.java
+++ java/src/logical/arrays/imp/Palindrome_Number.java
@@ -1,4 +1,27 @@
 package logical.arrays.imp;
 
 public class Palindrome_Number {
+
+    public static boolean isPalindrome(int num){
+        System.out.print("Input => "+num+" :: ");
+        if(num < 0)
+            return false;
+        int original = num;
+        int reversed = 0;
+        while(num != 0){
+            int lastDigit = num % 10;
+            reversed = reversed * 10 + lastDigit;
+            num = num/10;
+        }
+        return original == reversed;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121)); //true
+        System.out.println(isPalindrome(101)); // true
+        System.out.println(isPalindrome(0)); //true
+        System.out.println(isPalindrome(123)); //false
+        System.out.println(isPalindrome(-121)); //false
+
+    }
 }
